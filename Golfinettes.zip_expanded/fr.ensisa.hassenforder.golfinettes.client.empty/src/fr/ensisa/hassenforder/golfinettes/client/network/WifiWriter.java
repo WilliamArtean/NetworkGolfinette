22f2c44b4f2fe3e2a37cc5be733dbad497df89d8
papichou <valovaly6@gmail.com>
2020-01-14 10:59:03 +0100

    Update WifiWriter.java

--- Golfinettes.zip_expanded/fr.ensisa.hassenforder.golfinettes.client.empty/src/fr/ensisa/hassenforder/golfinettes/client/network/WifiWriter.java
+++ Golfinettes.zip_expanded/fr.ensisa.hassenforder.golfinettes.client.empty/src/fr/ensisa/hassenforder/golfinettes/client/network/WifiWriter.java
@@ -12,9 +12,37 @@ import fr.ensisa.hassenforder.network.BasicAbstractWriter;
 
 public class WifiWriter extends BasicAbstractWriter {
 
-    public WifiWriter(OutputStream outputStream) {
+    public int i = 0;
+    public WifiWriter(OutputStream outputStream) throws IOException {
         super(outputStream);
+       
     }
 
+    
+    private void writeAsByte(int value) {
+        writeByte((byte) (value & 0xFF));
+	}
+   
+    public void writeWifimsg(List<Event> events) {
+    	
+    	//envoi le nombre d'events au serveur
+    	writeAsByte(events.size());
+    	
+		while(events.size()>i)
+		{
+		
+		writeLong(events.get(i).getId());
+		writeLong(events.get(i).getTimestamp().getTime());
+		writeShort((short) events.get(i).getLocation().getLatitude());
+		writeShort((short) events.get(i).getLocation().getLongitude());
+		writeShort((short) events.get(i).getBattery().getTemperature());
+		writeAsByte(events.get(i).getBattery().getLoad());
+		writeAsByte(events.get(i).getBattery().getLoadingCurrent());
+		writeAsByte(events.get(i).getLocation().getTemperature());
+		writeAsByte(events.get(i).getLocation().getHumidity());
+		i++;
+		}
+		
+	}
 
 }
