cd56207a3e8544b5f03a563890bdbc961cb5e9d4
Pierre M <49361083+WilliamArtean@users.noreply.github.com>
2020-01-14 10:24:41 +0100

    Protocol
    Ajout de la syntaxe du protocole

--- Golfinettes.zip_expanded/fr.ensisa.hassenforder.golfinettes.common.empty/src/fr/ensisa/hassenforder/golfinettes/network/Protocol.java
+++ Golfinettes.zip_expanded/fr.ensisa.hassenforder.golfinettes.common.empty/src/fr/ensisa/hassenforder/golfinettes/network/Protocol.java
@@ -7,4 +7,35 @@ public class Protocol {
 
     // left to help you (or not)
 	public static final int SIGFOX_STD				= 0x01;
+	public static final int WIFI_STD				= 0x02;
+	
+	// wifi
+	 public static final int REQUEST_CONNECTION = 0;
+	 public static final int SENDING_ID = 0x0111;
+	 public static final int SENDING_PASSWORD = 0x0112;
+	 
+	 
+	 
+	 public static final int REPLY_OK = 0x0210;
+	 public static final int REPLY_KO = 0x0220;
+	 public static final int REPLY_ID = 0;
+	 public static final int REPLY_PASSWORD = 0x0240;
+	 
+	 public static final int SYGFOX_ALM_ZIIN = 1;
+	 public static final int SYGFOX_ALM_ZIOUT = 2;
+	 public static final int SYGFOX_ALM_EXVITESSE = 3;
+	 public static final int SYGFOX_ALM_DISTMAX = 4;
+	 public static final int SYGFOX_ALM_CLIM= 5;
+	 public static final int SYGFOX_ALM_BATR=6;
+	 public static final int SYGFOX_ALM_MAT=7;
+	 public static final int SYGFOX_ALM_SOFT=8;
+	 public static final int SYGFOX_ALM_MALCL=9;
+	 public static final int SYGFOX_ALM_UNAUTH=10;
+	 
+	 public static final int SYGFOX_EMP=11;
+	 
+	 
+	 
+	 
+	 
 }
