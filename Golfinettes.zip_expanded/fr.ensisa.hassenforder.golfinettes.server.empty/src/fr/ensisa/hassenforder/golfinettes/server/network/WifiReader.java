33b77eaaba2d6c11ef65da925852ade68ce758f1
Omar Ch <40161287+Winssench@users.noreply.github.com>
2020-01-16 18:06:05 +0100

    Update WifiReader.java

--- Golfinettes.zip_expanded/fr.ensisa.hassenforder.golfinettes.server.empty/src/fr/ensisa/hassenforder/golfinettes/server/network/WifiReader.java
+++ Golfinettes.zip_expanded/fr.ensisa.hassenforder.golfinettes.server.empty/src/fr/ensisa/hassenforder/golfinettes/server/network/WifiReader.java
@@ -1,6 +1,7 @@
 package fr.ensisa.hassenforder.golfinettes.server.network;
 
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,69 +18,138 @@ import fr.ensisa.hassenforder.network.BasicAbstractReader;
 
 public class WifiReader extends BasicAbstractReader {
 
+	private static final int MESSAGE_0 = 0;
 	private String versionCode;
 	private List<Event> events;
 	private Version version;
 	private long id;
-	private String kind;
+	private String kind ="wifi";
 	
 	public WifiReader(InputStream inputStream) {
 		super (inputStream);
+		this.events = new ArrayList<>();
+	}
+	
+	public Event readData()
+	{
+		int id = (int)readLong();
+		//System.out.println("reader id" + id);
+		int borrower = readInt();
+		int usageEvent = readInt();
+		int usageState = readInt();
+		int detail = readInt();
+		int usAlarme = readInt();
+		short longitude = readShort();
+//		System.out.println("longitude :  " + longitude);
+		short latitude = readShort();
+//		System.out.println("latitude : " + latitude);
+		short btrTemp = readShort();
+//		System.out.println("baterie temperature : " + btrTemp);
+		int btrload = readByte();
+//		System.out.println("batterie load : " + btrload);
+		int btrCurrent = readByte();
+//		System.out.println("btrCurrent : " + btrCurrent);
+		int btrDischarge = readByte();
+//		System.out.println("btrDischarge : " + btrDischarge);
+		int btrMode = readByte();
+//		System.out.println("btrMode : " + btrMode);
+		int ambTemp = readByte();
+//		System.out.println("ambTemp : " + ambTemp);
+		int ambHumidity = readByte();
+//		System.out.println("ambHumidity : " + ambHumidity);
+		boolean free = readBoolean();
+//		System.out.println(" freeMode : " + free);
+		long date = readLong();
+//		System.out.println(" date :  " +  new SimpleDateFormat("hh:mm:ss").format(date));
+		Usage usage = new Usage(borrower,Usage.BorrowerEvent.values()[usageEvent],
+								Usage.UsageState.values()[usageState],
+								detail,
+								usAlarme);
+		
+		Event e = new Event(ambHumidity, date, kind);
+		e.withUsage(usage);
+		e.withLocation(new Location(latitude, longitude, ambTemp, ambHumidity));
+		e.withBattery(new Battery(btrTemp, btrload,btrCurrent ,btrDischarge , Battery.BatteryMode.values()[btrMode]));
+		return e;
 	}
 
 	public void receive() {
-		type = readInt ();
-		switch (type) {
-		case 0 : break;
-		case 1 : break;
-		case 2 : break;
-		case 3 : break;
-		case 4 : break;
-		case 5 : break;
-		case 6 : break;
-		case 7 : break;
-		case 8 : break;
-		case 9 : break;
-		case 10 : break;
-		case 11 : break;
-		case 13 : 
+		System.out.println("receiver");
+		int size = readInt();
+		System.out.println("receiver size" + size);
+		int i = 0;
+		while ( i <= size && size <15) 
 		{
-			int size = this.readInt();
-	    	this.versionCode = this.readString();
-	    	String content1= this.readString();
-	    	byte [] content2 = new byte[size];
-	    	for (int i=0;i< content2.length;i++)
-	    	{
-	    		content2[i] = this.readByte();
-	    	}
-	    	this.version = new Version(this.versionCode,content1,content2);
+			type = readInt();
+//			type = 15 ;
+//			System.out.println("type moda" + type);
+			switch (type) {
+			case Protocol.WIFI_STD :
+				int id = (int)readLong();
+				//System.out.println("reader id" + id);
+				
+				
+				int borrower = readInt();
+				int usageEvent = readInt();
+				int usageState = readInt();
+				int detail = readInt();
+				int usAlarme = readInt();
+				
+				
+				
+				
+//				System.out.println("sysUsAlarme : " + usAlarme);
+				short longitude = readShort();
+//				System.out.println("longitude :  " + longitude);
+				short latitude = readShort();
+//				System.out.println("latitude : " + latitude);
+				short btrTemp = readShort();
+//				System.out.println("baterie temperature : " + btrTemp);
+				int btrload = readByte();
+//				System.out.println("batterie load : " + btrload);
+				int btrCurrent = readByte();
+//				System.out.println("btrCurrent : " + btrCurrent);
+				int btrDischarge = readByte();
+//				System.out.println("btrDischarge : " + btrDischarge);
+				int btrMode = readByte();
+//				System.out.println("btrMode : " + btrMode);
+				int ambTemp = readByte();
+//				System.out.println("ambTemp : " + ambTemp);
+				int ambHumidity = readByte();
+//				System.out.println("ambHumidity : " + ambHumidity);
+				boolean free = readBoolean();
+//				System.out.println(" freeMode : " + free);
+				long date = readLong();
+//				System.out.println(" date :  " +  new SimpleDateFormat("hh:mm:ss").format(date));
+				Usage usage = new Usage(borrower,Usage.BorrowerEvent.values()[usageEvent],
+										Usage.UsageState.values()[usageState],
+										detail,
+										usAlarme);
+				
+				Event e = new Event(ambHumidity, date, kind);
+				e.withUsage(usage);
+				e.withLocation(new Location(latitude, longitude, ambTemp, ambHumidity));
+				e.withBattery(new Battery(btrTemp, btrload,btrCurrent ,btrDischarge , Battery.BatteryMode.values()[btrMode]));
+				System.out.println("i read "+ i + "event : " + e.toString());
+				this.events.add(e);
+	
+				
+			}
+			i++;
+			
+//			System.out.println("toute la liste quoi");
+//			for(Event e : events)
+//			{
+//				System.out.println(e.toString());
+//			}
+//			System.out.println("fin -----------------");
+			
 		}
-		case 14 : 
-		{
-			int size = this.readInt();
-	    	this.versionCode = this.readString();
-	    	String content1= this.readString();
-	    	byte [] content2 = new byte[size];
-	    	for (int i=0;i< content2.length;i++)
-	    	{
-	    		content2[i] = this.readByte();
-	    	}
-	    	this.version = new Version(this.versionCode,content1,content2);
-		}
-		case 15 : 
-		{
-			int size = this.readInt();
-	    	this.versionCode = this.readString();
-	    	String content1= this.readString();
-	    	byte [] content2 = new byte[size];
-	    	for (int i=0;i< content2.length;i++)
-	    	{
-	    		content2[i] = this.readByte();
-	    	}
-	    	this.version = new Version(this.versionCode,content1,content2);
-		}
-		}
+		
+		
 	}
+
+	
 
 	public String getVersionCode() {
 		return versionCode;
